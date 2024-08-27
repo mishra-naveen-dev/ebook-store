f2b0a1b702ccf2450ee8016f2feaf5e98bddf469
nvineng <naveen211063cse@sdbc.ac.in>
2024-08-27 14:51:36 +0530

    home page is going

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import './Animations.css'
 import { Navbar, Footer } from './components/index.js';
 import React from 'react';
-import HomePage from './pages/Home.jsx';
+// import Home from './pages/Home.jsx';
+import Home from './pages/Home.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './pages/AboutUs.jsx';
-import Contact from './pages/Contact.jsx'
+import Contact from './pages/Contact.jsx';
+
+
 
 function App() {
   return (
@@ -12,7 +16,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<HomePage />} />
+          <Route path="/" exact element={<Home />} /> {/* This ensures Home is the default */}
           <Route path="/about" exact element={<About />} />
           <Route path="/contact" exact element={<Contact />} />
         </Routes>
@@ -21,5 +25,6 @@ function App() {
     </Router>
   );
 }
+
 
 export default App;
---
+++ src/components/Preloader.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import './Preloader.css'
+const Preloader = () => {
+    const [isVisible, setIsVisible] = useState(true);
+    const [fadeOut, setFadeOut] = useState(false);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setFadeOut(true);
+            setTimeout(() => setIsVisible(false), 300);
+
+        }, 2500);
+        return () => clearTimeout(timer);
+
+    }, []);
+    return (
+        isVisible && (
+            <div className={`preloader ${fadeOut ? 'fade-out' : ''}`}>
+                <video autoPlay muted loop className="preloder-vedio">
+                    <source src="/preload.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        )
+    )
+}
+export default Preloader;
